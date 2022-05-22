9456f7fd60501fe35b34bf5285a36589b659a761
Terry Zheng <terryzhengtx@gmail.com>
2022-05-22 16:44:23 -0700

    Use constants for task commands

--- dev/main.py
+++ dev/main.py
@@ -1,7 +1,8 @@
 import argparse
 
 from dev.clean import CleanTask
-from dev.constants import RC_OK
+from dev.constants import (RC_OK, TASK_CLEAN, TASK_INSTALL, TASK_LINT,
+                           TASK_LIST, TASK_TEST, TASK_UNINSTALL)
 from dev.install import InstallTask
 from dev.lint import LintTask
 from dev.test import TestTask
@@ -13,21 +14,19 @@ def main() -> int:
         prog="dev",
         description="Dev tools CLI for performing common development tasks.",
     )
-    parser.add_argument(
-        "action", choices=["clean", "install", "uninstall", "test", "lint"]
-    )
+    parser.add_argument("action", choices=TASK_LIST)
     args = parser.parse_args()
     rc = RC_OK
 
-    if args.action == "clean":
+    if args.action == TASK_CLEAN:
         rc = CleanTask.execute()
-    elif args.action == "install":
+    elif args.action == TASK_INSTALL:
         rc = InstallTask.execute()
-    elif args.action == "uninstall":
+    elif args.action == TASK_UNINSTALL:
         rc = UninstallTask.execute()
-    elif args.action == "test":
+    elif args.action == TASK_TEST:
         rc = TestTask.execute()
-    elif args.action == "lint":
+    elif args.action == TASK_LINT:
         rc = LintTask.execute()
 
     return rc

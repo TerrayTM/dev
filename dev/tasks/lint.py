c10a7b2d133d85bf92514394fbc43289d0700254
Terry Zheng <terryzhengtx@gmail.com>
2022-08-27 21:09:07 -0400

    Improve linting

--- dev/tasks/lint.py
+++ dev/tasks/lint.py
@@ -1,10 +1,11 @@
 from argparse import ArgumentParser, Namespace, _SubParsersAction
+from io import StringIO
 from pathlib import Path
 
 import isort
-from black import FileMode, WriteBack, format_file_in_place
+from black import FileMode, InvalidInput, WriteBack, format_file_in_place
 
-from dev.constants import RC_OK
+from dev.constants import RC_FAILED, RC_OK
 from dev.files import filter_python_files, get_changed_repo_files, get_repo_files
 from dev.tasks.task import Task
 
@@ -13,18 +14,31 @@ class LintTask(Task):
     def _perform(self, args: Namespace) -> int:
         get_files_function = get_repo_files if args.all else get_changed_repo_files
         files = get_files_function([filter_python_files])
-        formatted = 0
+        write_back = WriteBack.NO if args.verify else WriteBack.YES
+        output = StringIO() if args.verify else None
+        formatted = []
 
         for file in files:
-            if format_file_in_place(
-                Path(file), False, FileMode(), WriteBack.YES
-            ) | isort.file(file, profile="black"):
-                formatted += 1
+            try:
+                if format_file_in_place(
+                    Path(file), False, FileMode(), write_back
+                ) | isort.file(file, output=output, profile="black", quiet=True):
+                    formatted.append(file)
+            except InvalidInput:
+                print(f"Cannot parse Python file '{file}'.")
+                return RC_FAILED
 
-        if formatted > 0:
+        if len(formatted) > 0:
+            if args.verify:
+                print("The following files are misformatted:")
+                for file in formatted:
+                    print(f"  - {file}")
+
+                return RC_FAILED
+
             print(
                 f"Checked {len(files)} file{'s' if len(files) > 1 else ''} and "
-                f"formatted {formatted} file{'s' if formatted > 1 else ''}."
+                f"formatted {len(formatted)} file{'s' if len(formatted) > 1 else ''}."
             )
 
         return RC_OK
@@ -33,5 +47,6 @@ class LintTask(Task):
     def _add_task_parser(cls, subparsers: _SubParsersAction) -> ArgumentParser:
         parser = super()._add_task_parser(subparsers)
         parser.add_argument("--all", action="store_true", dest="all")
+        parser.add_argument("--verify", action="store_true", dest="verify")
 
         return parser

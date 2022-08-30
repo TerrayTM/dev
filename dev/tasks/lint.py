32c4dc39dc137d56d9dbb7a4129f2c8c2d77ea17
Terry Zheng <terryzhengtx@gmail.com>
2022-08-29 22:24:54 -0400

    Check for line length when linting

--- dev/tasks/lint.py
+++ dev/tasks/lint.py
@@ -11,22 +11,39 @@ from dev.tasks.task import Task
 
 
 class LintTask(Task):
+    def _validate_line_limit(self, file: str) -> bool:
+        result = True
+
+        with open(file) as reader:
+            for line_number, line in enumerate(reader, 1):
+                if len(line.rstrip("\n")) > 88:
+                    result = False
+                    print(
+                        f"File '{file}' on line {line_number} exceeds the "
+                        "width limit of 88 characters."
+                    )
+
+        return result
+
     def _perform(self, all_files: bool = False, validate: bool = False) -> int:
         get_files_function = get_repo_files if all_files else get_changed_repo_files
         files = get_files_function([filter_python_files])
         write_back = WriteBack.NO if validate else WriteBack.YES
         output = StringIO() if validate else None
-        formatted = []
+        formatted = set()
 
         for file in files:
             try:
                 if format_file_in_place(
                     Path(file), False, FileMode(), write_back
                 ) | isort.file(file, output=output, profile="black", quiet=True):
-                    formatted.append(file)
+                    formatted.add(file)
             except InvalidInput:
                 print(f"Cannot parse Python file '{file}'.")
                 return RC_FAILED
+
+            if not self._validate_line_limit(file) and validate:
+                formatted.add(file)
 
         if len(formatted) > 0:
             if validate:

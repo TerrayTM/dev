4bf4c80be1dfe6ffb3cc949432a8db14a52b83ca
Terry Zheng <terryzhengtx@gmail.com>
2022-09-02 21:58:14 -0700

    Fix type annotation

--- dev/tasks/__init__.py
+++ dev/tasks/__init__.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from dev.exceptions import TaskNotFoundError
 from dev.tasks.build import BuildTask
 from dev.tasks.clean import CleanTask
@@ -7,6 +9,7 @@ from dev.tasks.install import InstallTask
 from dev.tasks.lint import LintTask
 from dev.tasks.publish import PublishTask
 from dev.tasks.run import RunTask
+from dev.tasks.task import Task
 from dev.tasks.test import TestTask
 from dev.tasks.time import TimeTask
 from dev.tasks.uninstall import UninstallTask
@@ -28,7 +31,7 @@ __all__ = [
 _task_map_cache = None
 
 
-def get_task(name: str) -> type:
+def get_task(name: str) -> Type[Task]:
     global _task_map_cache
 
     if _task_map_cache is None:

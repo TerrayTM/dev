392d88e7bdc78faad9ff8a37c0e053981fe27bc8
Terry Zheng <terryzhengtx@gmail.com>
2023-01-27 01:02:42 -0500

    Add unused task to chain task

--- dev/tasks/chain.py
+++ dev/tasks/chain.py
@@ -2,6 +2,7 @@ from dev.tasks.doc import DocTask
 from dev.tasks.lint import LintTask
 from dev.tasks.spell import SpellTask
 from dev.tasks.task import Task
+from dev.tasks.unused import UnusedTask
 
 
 class ChainTask(Task):
@@ -9,6 +10,7 @@ class ChainTask(Task):
         return max(
             [
                 LintTask.execute(),
+                UnusedTask.execute(),
                 DocTask.execute(validate=True, ignore_missing=True),
                 SpellTask.execute(),
             ]

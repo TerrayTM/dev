b2bb5ffd8814325e84455cc39f8850a39ea6d58c
Terry Zheng <terryzhengtx@gmail.com>
2023-02-03 18:12:24 -0500

    Add linter for C#

--- dev/tasks/lint.py
+++ dev/tasks/lint.py
@@ -4,12 +4,13 @@ from typing import List, Optional
 from dev.constants import ReturnCode
 from dev.exceptions import LinterError
 from dev.files import build_file_extensions_filter, select_get_files_function
+from dev.linters.csharp import CSharpLinter
 from dev.linters.javascript import JavaScriptLinter
 from dev.linters.python import PythonLinter
 from dev.output import output
 from dev.tasks.task import Task
 
-_INSTALLED_LINTERS = (PythonLinter, JavaScriptLinter)
+_INSTALLED_LINTERS = (PythonLinter, JavaScriptLinter, CSharpLinter)
 _SUPPORTED_EXTENSIONS = tuple(linter.get_extension() for linter in _INSTALLED_LINTERS)
 
 
